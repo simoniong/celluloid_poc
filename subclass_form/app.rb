73a78bfc04b363c69f586ae7ab7e2bc2e4c4cbc1
Simon Iong <manin.iong@gmail.com>
2018-01-20 18:38:02 +0800

    test subclass_form

---
+++ subclass_form/app.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'celluloid/autostart'
+require 'reel'
+
+class MyServer < Reel::Server::HTTP
+  def initialize(host = "127.0.0.1", port = 3000)
+    super(host, port, &method(:on_connection))
+  end
+
+  def on_connection(connection)
+    connection.each_request do |request|
+      if request.websocket?
+        handle_websocket(request.websocket)
+      else
+        handle_request(request)
+      end
+    end
+  end
+
+  def handle_request(request)
+    request.respond :ok, "Hello, world!"
+  end
+
+  def handle_websocket(sock)
+    sock << "Hello everyone out there in WebSocket land!"
+    sock.close
+  end
+end
+
+MyServer.run
