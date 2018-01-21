16f193597cf4e91ff38496da20f9ee94f8bf4757
Simon Iong <manin.iong@gmail.com>
2018-01-21 17:19:53 +0800

    add more function to echo_app

--- subclass_form/echo_app.rb
+++ subclass_form/echo_app.rb
@@ -3,17 +3,58 @@ require 'bundler/setup'
 require 'celluloid/autostart'
 require 'reel'
 
+class SocketHandler
+  include Celluloid
+  include Celluloid::Logger
+
+  def handle(socket)
+    while true do
+      msg = socket.read
+      info "got msg: #{msg}"
+
+      if msg == 'stop'
+        socket.close
+        break
+      else
+        info 'echo back'
+        socket << "echo back: #{msg}"
+        info 'done'
+      end
+    end
+  rescue Reel::SocketError, EOFError
+    info 'client disconnect somehow'
+    socket.close
+  end
+
+  def notify_rolling_restart
+    info 'notify rolling restart inside actor'
+  end
+end
+
 class MyServer < Reel::Server::HTTP
   include Celluloid::Logger
 
   def initialize(host = "127.0.0.1", port = 3000)
     super(host, port, &method(:on_connection))
+    @pool = SocketHandler.pool(size: 2)
+  end
+
+  def notify_rolling_restart
+    info 'got SIGTERM'
+    @pool.notify_rolling_restart
+    # @pool.actors.each do |actor|
+    #   actor.notify_rolling_restart
+    # end
   end
 
   def on_connection(connection)
     connection.each_request do |request|
+      info 'running each_request'
       if request.websocket?
-        handle_websocket(request.websocket)
+        socket = request.websocket
+        socket << "welcome to socket world"
+        @pool.handle socket
+        info 'finished handling websocket'
       else
         handle_request(request)
       end
@@ -23,14 +64,13 @@ class MyServer < Reel::Server::HTTP
   def handle_request(request)
     request.respond :ok, "Hello, world!"
   end
+end
 
-  def handle_websocket(sock)
-    sock << "Hello everyone out there in WebSocket land!"
-    msg = sock.read
-    info "got msg: #{msg}"
-    sock << "echo back: #{msg}"
-    sock.close
+MyServer.supervise_as :server
+Signal.trap('SIGTERM') do
+  Thread.new do
+    Celluloid::Actor[:server].notify_rolling_restart
   end
 end
 
-MyServer.run
+sleep
