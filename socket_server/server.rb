799e5701d83d73367f7d2bacffbc7d085374302e
Simon Iong <manin.iong@gmail.com>
2018-01-22 00:44:38 +0800

    create a simple socket server

---
+++ socket_server/server.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'reel'
+require 'json'
+
+class WebServer < Reel::Server
+  include Celluloid::Logger
+
+  def initialize(host = "127.0.0.1", port = 3000)
+    info "Server starting on #{host}:#{port}"
+    super(host, port, &method(:on_connection))
+  end
+
+  def on_connection(connection)
+    while request = connection.request
+      case request
+      when Reel::Request
+        route_request connection, request
+      when Reel::WebSocket
+        info "Received a WebSocket connection"
+        route_websocket request
+      end
+    end
+  end
+
+  def route_request(connection, request)
+    info "404 Not Found: #{request.path}"
+    connection.respond :not_found, "Not found"
+  end
+
+  def route_websocket(socket)
+    if socket.url == "/games"
+      SocketHandler.new(socket)
+    else
+      info "Received invalid WebSocket request for: #{socket.url}"
+      socket.close
+    end
+  end
+end
+
+WebServer.supervise_as :reel
+
+sleep
