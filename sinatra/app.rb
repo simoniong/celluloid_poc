ff865d02916ad1e50cbcac1f8c88cba74def94c1
Simon Iong <manin.iong@gmail.com>
2018-01-24 11:05:27 +0800

    draft signal handling

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -28,9 +28,13 @@ Celluloid::Actor[:slow_stage_pool] = StageWorker.pool(size: 100, args:[:slow])
 Celluloid::Actor[:fast_stage_pool] = StageWorker.pool(size: 100, args:[:fase])
 
 class App < Sinatra::Base
+  # disable backend server level signal handling
+  set :server_settings, { signals: false }
+  # disable sinatra level signal handling
+  set :traps, false
+
   set :status, 1
   set :restarting, EM::Channel.new
-  set :traps, false
   get '/' do
     
     headers 'Access-Control-Allow-Origin' => '*'
@@ -62,61 +66,13 @@ class App < Sinatra::Base
       erb :index
     end
   end
-  
-  # trap('SIGTERM') do
-  #   puts "receiving SIGTERM inside"
-  #   self.settings.restarting.push("restarting")
-  # end
-  #
-#trap("SIGKILL") { puts "KILL" }
-#Signal.trap("EXIT") { puts "EXIT" }
 
-#Signal.trap("ABRT") { puts "ABRT" }
-#at_exit { App.settings.restarting.push("restarting") }
-# def self.quit!
-#  $stdout.puts "sending restarting ... "
-#  self.settings.restarting.push("restarting")
-#  super
-# end
-
-end
-
-# trap(:TERM) do 
-#   puts "receiving SIGTERM outside"
-#   self.settings.restarting.push("restarting")
-# end
-#
-
-#  App.settings.restarting.push("Restarting")
-
-module Thin
-  class Server
-
-    def stop
-      if running?
-        @backend.stop
-        unless @backend.empty?
-          log_info "Waiting for #{@backend.size} connection(s) to finish, "\
-                   "can take up to #{timeout} sec, CTRL+C to stop now"
-        end
-      else
-        stop!
-      end
-    end
-
-		# def handle_signals
-		# 	case @signal_queue.shift
-		# 	when 'INT'
-		# 		stop!
-		# 	when 'TERM', 'QUIT'
-		# 		stop
-		# 	when 'HUP'
-		# 		restart
-		# 	when 'USR1'
-		# 		reopen_log
-		# 	end
-		# 	EM.next_tick { handle_signals } unless @signal_queue.empty?
-		# end
+  trap('TERM') do
+    puts 'info: Got SIGTERM singal'
+    puts 'notify subscriber to restarting'
+    self.settings.restarting.push("restarting")
+    puts 'done'
+    super
   end
 end
 
